effd0b2c6d51948e690095470babbf7d5fd87e4d
Brendan Ronan <brendan.ronan@cengage.com>
2014-07-14 14:51:39 -0400

    working with full functionality, outputs csv

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import getpass
+import csv
 from bs4 import BeautifulSoup
 
 s = requests.Session()
@@ -30,12 +31,9 @@ htmlFile.close()
 soup = BeautifulSoup(open("glossary.html"))
 soup.prettify()
 
-htmlFile = open("glossary.html", "w")
-htmlFile.write(soup.find("div", {"class": "wiki-content"}).prettify().encode('utf-8'))
-htmlFile.close()
-
 tabledata = soup.findAll("td", {"class": "confluenceTd"})
 
+# create a dictionary from tabledata of glossary terms/definitions
 count = 1
 d = {}
 key = ""
@@ -43,16 +41,21 @@ key = ""
 for data in tabledata:
 	if count%2 == 0:
 		soup1 = data.findAll("span")
-		soupList = []
-			for line in soup1 :
-			soupList += line
+		soupList = ""
+		for line in soup1:
+			soupList += line.string.encode('ascii', 'ignore')
 		d[key] = soupList
 		key = ""
 	else:
 		key = data.span.string
 	count +=1
 
-print(d)	
+print(d)
+
+# write dictionary to csv 
+with open('output.csv','wb') as f:
+    w = csv.writer(f)
+    w.writerows(d.items())	
 
 #melanie was here
 #phil was here
